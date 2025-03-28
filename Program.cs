07b91459380bdbf77eb8da2e1424b2c37ed30f0c
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 19:57:34 -0300

    Executado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿String trident;
+Console.WriteLine("escreva uma frase");
+trident=Console.ReadLine()!.Replace("a", "4")
+.Replace("e", "3")
+.Replace("i", "1")
+.Replace("l", "1")
+.Replace("o", "0")
+.Replace("t", "7")
+.Replace("s", "5").ToUpper();
+Console.WriteLine($"{trident}");
